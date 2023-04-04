2ca4ff3d004af425cd92e82ebca50d9fd66c7967
Will <02william.lin@gmail.com>
2023-04-04 09:41:49 -0400

    AddedBossMovement

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossHealthBar : MonoBehaviour
+public class BossHealth : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
