ff05870c3dbb2a75d70b5eb0af020bc08ff36758
Will <02william.lin@gmail.com>
2023-04-05 10:26:36 -0400

    CreatingBossRotation

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform player;
+
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
     {
-        
+        Vector3 flipped = transform; 
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f,180f,0f);
+            isFlipped = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/BossRun.cs
+++ Assets/Scripts/BossRun.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class BossRun : StateMachineBehaviour
 {
-    public float speed = 2.5f;
+    public float speed = 10f;
     Transform player;
     Rigidbody2D rb;
+    Boss boss;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.GetComponent<Boss>().LookAtPlayer();
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
@@ -28,13 +31,5 @@ public class BossRun : StateMachineBehaviour
     {
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
 }
