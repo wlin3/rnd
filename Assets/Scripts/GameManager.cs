7d4d947b2bc8a5c08bd2b9d573eeff6c9d4fc4c1
Alex <mrbubbleplayer@gmail.com>
2023-04-17 09:58:28 -0400

    Started Working on Upgrade Gui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameData
     public int testPoints;
     public bool canTeleport;
     public int stagesWon;
+    public int upgradePoints;
     // Add any other variables or upgrades you want to save/load
 }
 
@@ -26,6 +27,10 @@ public class GameManager : MonoBehaviour
     public bool canTeleport = true;
 
     public int stagesWon;
+
+    public int upgradePoints;
+
+    public GameObject upgradeMenu;
 
     // Name of the GameManager GameObject
     private string mainGameManagerObjectName = "[Main] Game Manager"; // New name for the main GameManager object
@@ -67,6 +72,7 @@ public class GameManager : MonoBehaviour
         data.testPoints = testPoints;
         data.canTeleport = canTeleport;
         data.stagesWon = stagesWon;
+        data.upgradePoints = upgradePoints;
         // Add any other variables or upgrades you want to save
 
         // Serialize the data to binary format
@@ -92,6 +98,7 @@ public class GameManager : MonoBehaviour
             testPoints = data.testPoints;
             canTeleport = data.canTeleport;
             stagesWon = data.stagesWon;
+            upgradePoints = data.upgradePoints;
             // Set any other variables or upgrades from the loaded data
         }
     }
@@ -115,7 +122,8 @@ public class GameManager : MonoBehaviour
 
         else
         {
-            Debug.Log("You beat stage");
+            upgradeMenu.SetActive(true);
+            //Debug.Log("You beat stage");
         }
     }
 
@@ -128,8 +136,9 @@ public class GameManager : MonoBehaviour
             Debug.Log("Save data deleted");
 
             testPoints = 0;
-            canTeleport = true;
+            canTeleport = false;
             stagesWon = 0;
+            upgradePoints = 100;
         }
         else
         {
@@ -140,13 +149,19 @@ public class GameManager : MonoBehaviour
     public void DeleteRunData()
     {
         testPoints = 0;
-        canTeleport = true;
+        canTeleport = false;
         stagesWon = 0;
+        upgradePoints = 100;
         Debug.Log("Reset Run Data");
     }
 
     public int GetWins()
     {
         return stagesWon;
+    }
+
+    public void ClickTest()
+    {
+        Debug.Log("Button Clicked");
     }
 }
