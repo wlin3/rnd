71d67940de8f9eae10ee2b8b102707c6d4966bf4
Alex <mrbubbleplayer@gmail.com>
2023-04-19 09:38:28 -0400

    Nerfed Arrow and added death

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -26,18 +26,18 @@ public class Jumping : MonoBehaviour
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded && isJumping == false)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded && isJumping == false)
         {
             isJumping = true;
             jumpTimeCounter = jumpTime;
             rb.velocity = new Vector2(rb.velocity.x, initialJumpForce);
         }
-        if((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space)))
+        if((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow)))
             {
                 isJumping = false;
             }
 
-        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && isJumping == true)
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) && isJumping == true)
         {
             if (jumpTimeCounter > 0)
             {
