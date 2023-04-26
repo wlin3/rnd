f391019e4ecfa949f1d9d9c18cc65b145b9ae6af
Alex <mrbubbleplayer@gmail.com>
2023-04-26 10:25:44 -0400

    Bug Fixes
    Started Reworking the Entire Cooldown and Ability System

---
+++ Assets/CooldownSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooldownSystem : MonoBehaviour
+{
+    private float basicTimer;
+    public bool basicCanAttack;
+    private float chargeTimer;
+    public bool chargedCanAttack;
+    private float secondTimer;
+    public bool secondCanAttack;
+    private float thirdTimer;
+    public bool thidCanAttack;
+    public void BasicCooldown(float cooldown)
+    {
+
+    }
+}
